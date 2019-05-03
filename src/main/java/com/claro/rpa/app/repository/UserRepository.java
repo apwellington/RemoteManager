3828eaef28b70ce153a7293f0e1fb7576afb5815
Wellington Adames <wellington.adames.p@gmail.com>
2019-05-03 16:20:46 -0400

    Oracle Driver Config

--- src/main/java/com/claro/rpa/app/repository/UserRepository.java
+++ src/main/java/com/claro/rpa/app/repository/UserRepository.java
@@ -4,7 +4,10 @@ import com.claro.rpa.app.model.RpaUser;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends CrudRepository<RpaUser, Integer> {
+    public Optional<RpaUser> findByUsername(String username);
 
 }
